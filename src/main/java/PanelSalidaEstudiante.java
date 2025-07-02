5529cdd5c0b72352fbb98444ef98623f1ae8c3f3
manuel-mejicano <manuelortizmejicano100@gmail.com>
2025-07-02 00:13:35 -0600

    Eliminacion de servlets

--- src/main/java/PanelSalidaEstudiante.java
+++ src/main/java/PanelSalidaEstudiante.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import modelo.EstudianteDAO;
+
 
 @WebServlet("/panelSalidaEstudiante")
 public class PanelSalidaEstudiante extends HttpServlet {
